fa529c079456a6faf9a25e2a009a672b2c93abe8
Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>
2023-10-12 13:18:18 +0100

    [PP][OPS-9763] strict building
    Signed-off-by: Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>

--- project/SbtCommands.scala
+++ project/SbtCommands.scala
@@ -8,4 +8,28 @@ object SbtCommands {
       s"""set javaOptions -= "-Dplay.http.router=testOnlyDoNotUseInAppConf.Routes"""" ::
       state
   }
+
+  /**
+   * This disables strict compiler options,
+   * such as wartremover, fatal warnings, and more.
+   * Use it during development you wish to postpone code cleaning.
+   */
+  val relax: Command = Command.command(
+    "relax") { state =>
+    state.globalLogging.full.info("Ok, I'll turn a blind eye to some shortcomings. Remember, I won't be so lenient on Jenkins!")
+    s"""set Global / strictBuilding := false"""
+    state
+  }
+
+  val strictBuilding: Command = Command.command("strictBuilding") { state =>
+    state.globalLogging.full.info("Turning on strict building")
+    s"""set Global / strictBuilding := true"""
+    state
+  }
+
+  val commands: Seq[Command] = Seq(
+    runTestOnlyCommand,
+    relax,
+    strictBuilding
+  )
 }
