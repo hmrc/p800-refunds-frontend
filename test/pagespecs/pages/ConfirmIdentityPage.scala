3e8355c3fd34ce408d5b6ab4d5de1d3eb528ec76
tp10 <1626193+tp10@users.noreply.github.com>
2023-11-07 16:03:51 +0000

    [TP][OPS-11152] Build basic page structure (#22)

---
+++ test/pagespecs/pages/ConfirmIdentityPage.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pagespecs.pages
+
+import org.openqa.selenium.WebDriver
+import pagespecs.pagesupport.{ContentExpectation, Page, PageUtil}
+
+class ConfirmIdentityPage(baseUrl: String)(implicit webDriver: WebDriver) extends Page(
+  baseUrl,
+  path = "/get-an-income-tax-refund/we-need-you-to-confirm-your-identity"
+) {
+
+  override def expectedH1: String = "We need you to confirm your identity"
+
+  def assertPageIsDisplayed(errors: ContentExpectation*): Unit = withPageClue {
+
+    val contentExpectations: Seq[ContentExpectation] = Seq(ContentExpectation(
+      atXpath       = PageUtil.Xpath.mainContent,
+      expectedLines =
+        """
+          |We need you to confirm your identity
+          |Before we pay your refund, we need to ask you some security questions to confirm your identity.
+          |We will need to ask you for your:
+          |name
+          |address
+          |date of birth
+          |National Insurance number
+          |We do this to protect your security.
+          |""".stripMargin
+    )) ++ errors
+
+    PageUtil.assertPage(
+      path                = path,
+      h1                  = expectedH1,
+      title               = PageUtil.standardTitle(expectedH1),
+      contentExpectations = contentExpectations: _*
+    )
+  }
+
+}
