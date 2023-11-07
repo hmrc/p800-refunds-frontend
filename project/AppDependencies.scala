663493f8b6b2e1fbc2619f0e9c44c1711288bc49
tp10 <1626193+tp10@users.noreply.github.com>
2023-11-07 14:30:48 +0000

    [TP][Update dependencies and minor tweaks]

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,18 +5,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.22.0"
+  private val bootstrapVersion = "7.23.0"
   private val hmrcMongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     // format: OFF
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.24.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.26.0-play-28",
     "com.beachape"      %% "enumeratum-play"            % "1.7.3",
     "org.typelevel"     %% "cats-core"                  % "2.10.0",
     "org.julienrf"      %% "play-json-derived-codecs"   % "7.0.0", //choose carefully
-    "io.scalaland"      %% "chimney"                    % "0.8.1"
+    "io.scalaland"      %% "chimney"                    % "0.8.2"
   // format: ON
   )
 
@@ -29,9 +29,9 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"                  % "3.2.17",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2",
     "org.pegdown"             %  "pegdown"                    % "1.6.0",
-    "org.seleniumhq.selenium" %  "selenium-java"              % "4.14.1",
+    "org.seleniumhq.selenium" %  "selenium-java"              % "4.15.0",
     "org.seleniumhq.selenium" %  "htmlunit-driver"            % "4.13.0",
-    "org.wiremock"            %  "wiremock-standalone"        % "3.2.0",
+    "org.wiremock"            %  "wiremock-standalone"        % "3.3.1",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"
   // format: ON
   ).map(_ % Test)
