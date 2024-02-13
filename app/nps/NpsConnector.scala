5b0696a967e2b0909024b11784dd0e3f66579593
Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>
2024-02-13 09:05:02 +0000

    [PP][ops-11605] fix handling 422 responses
    Signed-off-by: Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>

--- app/nps/NpsConnector.scala
+++ app/nps/NpsConnector.scala
@@ -19,11 +19,13 @@ package nps
 import _root_.models.{Nino, P800Reference}
 import config.AppConfig
 import nps.models.{P800ReferenceCheckResultFailures, ReferenceCheckResult}
+import play.api.Logger
 import play.api.http.Status
+import play.api.libs.json.{JsError, JsResult, JsSuccess}
 import play.api.mvc.RequestHeader
 import requests.RequestSupport._
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.{HttpClient, HttpErrorFunctions, HttpReads, HttpResponse, UpstreamErrorResponse}
+import uk.gov.hmrc.http.{HttpClient, HttpErrorFunctions, HttpReads, HttpResponse, JsValidationException, UpstreamErrorResponse}
 import util.SafeEquals._
 
 import java.util.{Base64, UUID}
@@ -43,52 +45,7 @@ class NpsConnector @Inject() (
     s"/nps-json-service/nps/v1/api/reconciliation/p800/${nino.value}/${p800Reference.value}"
 
   def p800ReferenceCheck(nino: Nino, p800Reference: P800Reference)(implicit requestHeader: RequestHeader): Future[ReferenceCheckResult] = {
-
-    // A few notes:
-    // 1) Constructing Awkward HttpReads[ReferenceCheckResult] is necessary for mapping HttpResponse
-    // to ReferenceCheckResult. This mapping depends on the HTTP status codes and the content in the body.
-    // The body content varies based on the HTTP status. For instance:
-    //    - A 200 status code indicates a successful response, where the happy path JSON, if is valid, then it is transformed into P800ReferenceChecked.
-    //    - A 422 status code, if it contains specific JSON and an error code in it, is considered a valid response.
-    //    - A 404 status code is also treated as a valid case.
-    // 2) The need to cast to `ReferenceCheckResult` arises because HttpReads[A] is invariant in A.
-    // 3) To ensure compatibility with the error handling imposed by the Platform, HttpErrorFunctions are utilized.
-    // This ensures that exceptions thrown by handleResponseEither are captured and managed as expected.
-
-    implicit val reads: HttpReads[ReferenceCheckResult] = HttpReads.ask.flatMap {
-      case (method, url, response) => response.status match {
-        case Status.OK        => HttpReads[ReferenceCheckResult.P800ReferenceChecked].map((x: ReferenceCheckResult.P800ReferenceChecked) => x: ReferenceCheckResult)
-        case Status.NOT_FOUND => HttpReads.pure[ReferenceCheckResult](ReferenceCheckResult.ReferenceDidntMatchNino)
-        case Status.UNPROCESSABLE_ENTITY => HttpReads[P800ReferenceCheckResultFailures]
-          .map((checkResultFailures: P800ReferenceCheckResultFailures) =>
-            if (checkResultFailures.failures.exists(_.code === "TODO-refund-already-taken")) //TODO: agree with NPS the error code for this case
-              ReferenceCheckResult.RefundAlreadyTaken: ReferenceCheckResult
-            else
-              HttpErrorFunctions.handleResponseEither(method, url)(response) match {
-                case Left(err: UpstreamErrorResponse) => throw err
-                //this case should not happen as we already know that the status is 4xx
-                case Right(response: HttpResponse) =>
-
-                  throw UpstreamErrorResponse(
-                    message    = HttpErrorFunctions.upstreamResponseMessage(method, url, response.status, response.body),
-                    statusCode = response.status,
-                    reportAs   = Status.INTERNAL_SERVER_ERROR,
-                    headers    = response.headers
-                  )
-              })
-        case otherStatus => HttpErrorFunctions.handleResponseEither(method, url)(response) match {
-          case Left(err: UpstreamErrorResponse) => throw err
-          case Right(response: HttpResponse) =>
-            throw UpstreamErrorResponse(
-              message    = HttpErrorFunctions.upstreamResponseMessage(method, url, otherStatus, response.body),
-              statusCode = otherStatus,
-              reportAs   = Status.INTERNAL_SERVER_ERROR,
-              headers    = response.headers
-            )
-        }
-      }
-    }
-
+    implicit val reads: HttpReads[ReferenceCheckResult] = NpsConnector.reads
     httpClient
       .GET[ReferenceCheckResult](
         url     = p800ReferenceUrl(nino, p800Reference),
@@ -118,4 +75,67 @@ class NpsConnector @Inject() (
   private def makeOriginatorIdHeader(): (String, String) = {
     "gov-uk-originator-id" -> "DA2_MRA_DIGITAL"
   }
+
+}
+
+object NpsConnector {
+
+  // A few notes:
+  // 1) Constructing Awkward HttpReads[ReferenceCheckResult] is necessary for mapping HttpResponse
+  // to ReferenceCheckResult. This mapping depends on the HTTP status codes and the content in the body.
+  // The body content varies based on the HTTP status. For instance:
+  //    - A 200 status code indicates a successful response, where the happy path JSON, if is valid, then it is transformed into P800ReferenceChecked.
+  //    - A 422 status code, if it contains specific JSON and an error code in it, is considered a valid response.
+  //    - A 404 status code is also treated as a valid case.
+  // 2) The need to cast to `ReferenceCheckResult` arises because HttpReads[A] is invariant in A.
+  // 3) To ensure compatibility with the error handling imposed by the Platform, HttpErrorFunctions are utilized.
+  // This ensures that exceptions thrown by handleResponseEither are captured and managed as expected.
+
+  val reads: HttpReads[ReferenceCheckResult] = HttpReads.ask.flatMap {
+    case (method, url, response) => response.status match {
+      case Status.OK        => HttpReads[ReferenceCheckResult.P800ReferenceChecked].map((x: ReferenceCheckResult.P800ReferenceChecked) => x: ReferenceCheckResult)
+      case Status.NOT_FOUND => HttpReads.pure[ReferenceCheckResult](ReferenceCheckResult.ReferenceDidntMatchNino)
+      case Status.UNPROCESSABLE_ENTITY =>
+        logger.info(s"UNPROCESSABLE_ENTITY1: ${response.body}")
+        HttpReads[JsResult[P800ReferenceCheckResultFailures]]
+          .flatMap {
+            case JsSuccess(value: P800ReferenceCheckResultFailures, _) => HttpReads.pure(value)
+            case JsError(errors) =>
+              HttpReads.ask.map[P800ReferenceCheckResultFailures] {
+                case (method, url, _) =>
+                  throw new JsValidationException(method, url, this.getClass, errors.toString)
+              }
+          }.map { (checkResultFailures: P800ReferenceCheckResultFailures) =>
+            logger.info(s"UNPROCESSABLE_ENTITY: ${checkResultFailures.toString}")
+            if (checkResultFailures.failures.exists(_.code === "TODO-refund-already-taken")) //TODO: agree with NPS the error code for this case
+              ReferenceCheckResult.RefundAlreadyTaken: ReferenceCheckResult
+            else
+              HttpErrorFunctions.handleResponseEither(method, url)(response) match {
+                case Left(err: UpstreamErrorResponse) => throw err
+                //this case should not happen as we already know that the status is 4xx
+                case Right(response: HttpResponse) =>
+
+                  throw UpstreamErrorResponse(
+                    message    = HttpErrorFunctions.upstreamResponseMessage(method, url, response.status, response.body),
+                    statusCode = response.status,
+                    reportAs   = Status.INTERNAL_SERVER_ERROR,
+                    headers    = response.headers
+                  )
+              }
+          }
+      case otherStatus => HttpErrorFunctions.handleResponseEither(method, url)(response) match {
+        case Left(err: UpstreamErrorResponse) => throw err
+        case Right(response: HttpResponse) =>
+          throw UpstreamErrorResponse(
+            message    = HttpErrorFunctions.upstreamResponseMessage(method, url, otherStatus, response.body),
+            statusCode = otherStatus,
+            reportAs   = Status.INTERNAL_SERVER_ERROR,
+            headers    = response.headers
+          )
+      }
+    }
+  }
+
+  private lazy val logger = Logger(this.getClass)
+
 }
