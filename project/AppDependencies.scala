c9ed17b6026b380390978457434e7f8592f45e3e
Myles Offord <2564924+Lyrain@users.noreply.github.com>
2024-02-19 10:53:16 +0000

    [MO][OPS-11611] Update wiremock 3.3.1 -> 3.4.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -31,7 +31,7 @@ object AppDependencies {
     "org.pegdown"             %  "pegdown"                    % "1.6.0",
     "org.seleniumhq.selenium" %  "selenium-java"              % "4.17.0",
     "org.seleniumhq.selenium" %  "htmlunit-driver"            % "4.13.0",
-    "org.wiremock"            %  "wiremock-standalone"        % "3.3.1",
+    "org.wiremock"            %  "wiremock-standalone"        % "3.4.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"
   // format: ON
   ).map(_ % Test)
