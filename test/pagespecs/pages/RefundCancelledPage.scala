c68c5bad256e15912f5ebfb7092e79dc012d915e
tp10 <1626193+tp10@users.noreply.github.com>
2024-04-15 09:33:57 +0100

    [TP]OPS-11981

---
+++ test/pagespecs/pages/RefundCancelledPage.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pagespecs.pages
+
+import org.openqa.selenium.WebDriver
+import pagespecs.pagesupport.{ContentExpectation, Page, PageUtil}
+
+class RefundCancelledPage(baseUrl: String)(implicit webDriver: WebDriver) extends Page(
+  baseUrl,
+  path = "/get-an-income-tax-refund/refund-cancelled"
+) {
+
+  override def expectedH1: String = "Refund cancelled"
+  override def expectedTitleContent: String = "Refund cancelled"
+
+  def staticJourney: String = ""
+  override def assertPageIsDisplayed(extraExpectations: ContentExpectation*): Unit = withPageClue {
+
+    val contentExpectations: Seq[ContentExpectation] = Seq(ContentExpectation(
+      atXpath       = PageUtil.Xpath.mainContent,
+      expectedLines =
+        """
+          |Refund cancelled
+          |You have cancelled your refund request.
+          |No refund has been sent to your bank account.
+          |Try again
+          |Choose another way to get my refund
+          |""".stripMargin
+    )) ++ extraExpectations
+
+    PageUtil.assertPage(
+      baseUrl             = baseUrl,
+      path                = path,
+      h1                  = expectedH1,
+      title               = PageUtil.standardTitle(expectedTitleContent),
+      contentExpectations = contentExpectations: _*
+    )
+  }
+
+}
